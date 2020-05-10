27f0b7519924cf9b96e5d173cf073d0d46503194
Ryan Hua <ryan1117001@gmail.com>
2020-05-10 14:01:13 -0400

    Removed comments

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Routing from './src/Utils/Routing/index';
 
 export default function App() {
   return (
-    // <SafeAreaProvider>
       <Routing />
-    // </SafeAreaProvider>
   );
 }
